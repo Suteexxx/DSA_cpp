77d73947072aa875469dff587867ca322a0c8001
Suteekshn Manchanda <suteekshnman@gmail.com>
2026-07-07 10:19:52 +0530

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a=10, b=20;
+    cout<<"a + b = "<<a+b<<endl; // arithmetic operator
+    cout<<"a - b = "<<a-b<<endl;
+    cout<<"a * b = "<<a*b<<endl;
+    cout<<"a / b = "<<a/b<<endl;
+    cout<<"a % b = "<<a%b<<endl;
+    cout<<(10>20)<<endl; // relational operator
+    cout<<(10<20)<<endl;
+    cout<<(10>=20)<<endl;
+    cout<<(10<=20)<<endl;
+    cout<<(10==20)<<endl;
+    cout<<(10!=20)<<endl;
+    return 0;   
+}
---
+++ sum2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cout<<"enter a and b :";
+    cin>>a>>b;
+    cout<<"a + b = "<<a+b<<endl; // arithmetic operator
+    cout<<"a - b = "<<a-b<<endl;
+    cout<<"a * b = "<<a*b<<endl;
+    cout<<"a / b = "<<a/b<<endl;
+    cout<<"a % b = "<<a%b<<endl;
+    return 0;
+}
