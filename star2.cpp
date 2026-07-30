8a7e62cdb99a73a63ca135d4cfc452fe04220375
Suteekshn Manchanda <suteekshnman@gmail.com>
2026-07-30 09:41:22 +0530

    Add files via upload

---
+++ star2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int num=1;
+    cout<<"enter n";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<i+1;j++)
+        {
+            cout<<num<<" ";
+            num++;
+        }
+        cout<<endl;
+    }
+
+}
